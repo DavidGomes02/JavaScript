111ef30b12fe12017185313c57975f5dd0c19b41
David Gomes <david.henrique4560@gmail.com>
2024-01-26 11:35:42 -0300

    Vetores

---
+++ vetorArray.js
@@ -0,0 +1,38 @@
+//declara o vetor
+var vArray = [];
+
+//inicializa a array(vetor)
+console.log(vArray);
+
+//declare and initialize a array
+var num = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'];
+
+console.log(num[0]);
+console.log(num[1]);
+console.log(num[2]);
+console.log(num[3]);
+console.log(num[4]);
+console.log(num[5]);
+console.log(num[6]);
+console.log(num[7]);
+console.log(num[8]);
+console.log(num[9]);
+console.log(num[10]);
+console.log(num[11]);
+console.log(num[12]);
+console.log(num[13]);
+console.log(num[14]);
+console.log(num[15]);
+console.log(num[16]);
+console.log(num[17]);
+console.log(num[18]);
+console.log(num[19]);
+console.log(num[20]);
+
+//FORMA FACILITADA
+
+var nume = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'];
+var index;
+for (index=0; index<20; index++);{
+console.log(nume[index]);
+}
